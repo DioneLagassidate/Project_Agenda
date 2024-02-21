000715e17413a35ba32ccf02b943c1b874f09d1f
DLagassi <dmlagassi@outlook.com>
2024-02-20 22:39:36 -0300

    Atualização formul

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,33 +1,7 @@
-from typing import Any, Dict
+from django.shortcuts import render
 
-from django.db.models import Q
-from django.core.exceptions import ValidationError
-from django.shortcuts import get_object_or_404, redirect, render
-from django.core.paginator import Paginator
-from django import forms
+from contact.forms import ContactForm
 
-
-from contact.models import Contact
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-           'first_name', 'last_name', 'phone',
-        )
-    
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mesagem de erro',
-                code='invalid'
-            )
-        )
-        return super().clean()
 
 def create(request):
     if request.method == 'POST':
