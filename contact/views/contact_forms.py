8209c880a297b6fcf2ab1448c6bf90ae7f4ef578
DLagassi <dmlagassi@outlook.com>
2024-02-22 17:33:58 -0300

    Atualização fn

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from contact.forms import ContactForm
 from contact.models import Contact
 
-
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -17,7 +18,9 @@ def create(request):
     }
     
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
 
         return render(
@@ -35,10 +38,11 @@ def create(request):
            'contact/create.html',
             context
     )   
-
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True)
+        Contact, pk=contact_id, show=True, owner=request.user
+        )
 
     form_action = reverse('contact:update', args=(contact_id,))
 
@@ -70,10 +74,10 @@ def update(request, contact_id):
         'contact/create.html',
         context
     )
-
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
     confirmation = request.POST.get('confirmation', 'no')
 
